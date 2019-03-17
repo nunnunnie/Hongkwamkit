7ec18aae60283ffa7da003bb205a47a860e85ded
Kedsinee Junraksa <nunnunnie@gmail.com>
2019-03-17 14:23:46 +0700

    edit1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  gotoAbout(){
+    this.navCtrl.push(AboutPage);
   }
 
 }
